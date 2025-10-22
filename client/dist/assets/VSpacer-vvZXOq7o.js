7e82e5945545dfad4f78b367f8b6886c0cdbf311
sakuya474 <sakuya474@gmail.com>
2025-10-22 22:10:22 +0900

    Add built frontend files and fix .gitignore

---
+++ client/dist/assets/VSpacer-vvZXOq7o.js
@@ -0,0 +1,1 @@
+import{dl as s}from"./index-COHnkh6W.js";const e="/assets/images/logo.svg",o=s("v-spacer","div","VSpacer");export{o as V,e as _};
---
+++ client/dist/assets/ssrBoot-DOi1QZ1r.js
@@ -0,0 +1,1 @@
+import{s,z as t,x as e,c0 as n}from"./index-COHnkh6W.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
